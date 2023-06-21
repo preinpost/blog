32e004517a97eb70e73793f42a6055f49f314f2e
minsoo.lee <preinpost@gmail.com>
2023-06-22 00:32:59 +0900

    list layout 정리

--- src/app/blog/list/page.tsx
+++ src/app/blog/list/page.tsx
@@ -7,21 +7,29 @@ export default function page() {
 
   return (
     <>
-      <div className="flex flex-col">
-        <div>
+      <div className="flex flex-col items-center">
           {articleList.map((item) =>
             <Link
               key={item.id}
               href={`/blog/detail/${item.id}`}
-              className="hover:no-underline"
+              className="hover:no-underline w-2/3"
               style={{textDecoration:"none"}}
             >
-              <div className="p-5 my-10 border-2 rounded-xl border-rose-500 text-white" key={item.id}>
-                {item.meta?.title}
+              <div className="p-5 my-3 border-2 rounded-xl border-sky-400 text-white shadow-lg shadow-indigo-500/50" key={item.id}>
+                <div className="flex">
+                  <div className="mr-5 text-xl">{item.meta?.title}</div>
+
+                </div>
+
+                <div className="flex">
+                  <div className="ml-1">{item.meta?.date}</div>
+                  <div className="flex ml-auto">
+                    tag: {item.meta?.tag.map((tag) => <div key={item.id + tag} className="mx-1">{tag}</div>)}
+                  </div>
+                </div>
               </div>
             </Link>
           )}
-        </div>
       </div>
     </>
   )
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,12 +19,14 @@ export default function RootLayout({
     <html lang="en" className="markdown-body">
     <body className={`${inter.className} flex flex-col`}>
     <div className="flex flex-col my-8 w-11/12 h-auto self-center">
-      <div className="flex mb-16">
+      <div className="flex mb-36">
         <div className="flex justify-center items-center text-3xl mx-6 h-12">preinpost log</div>
         <div className="flex ml-auto">
-          <div className="flex justify-center items-center mx-6 h-12 w-28 rounded-lg border-2 border-rose-500">list
+          <div className="flex justify-center items-center mx-6 h-12 w-28 rounded-lg border-2 border-sky-400">
+            list
           </div>
-          <div className="flex justify-center items-center mx-6 h-12 w-28 rounded-lg border-2 border-rose-500">about
+          <div className="flex justify-center items-center mx-6 h-12 w-28 rounded-lg border-2 border-sky-400">
+            about
           </div>
         </div>
       </div>

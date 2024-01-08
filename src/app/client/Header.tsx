49094dfd3e7c680c768d652fe73e47cbaa68ba32
minsoo.lee <preinpost@gmail.com>
2024-01-09 00:41:32 +0900

    hamburger update

--- src/app/client/Header.tsx
+++ src/app/client/Header.tsx
@@ -17,12 +17,12 @@ export default function Header() {
         <div className="hidden justify-end items-center text-xl cursor-pointer text-grayishBlack font-bold dark:text-white md:flex" onClick={() => { router.push("/about") }}>
           about
         </div>
-        <button id="menu-btn" className="z-30 mt-2 block md:hidden focus:outline-none hamburger" onClick={onClickMobileMenu}>
-          <span className="hamburger-top bg-grayishBlack dark:bg-softWhite"></span>
-          <span className="hamburger-middle bg-grayishBlack dark:bg-softWhite"></span>
-          <span className="hamburger-bottom bg-grayishBlack dark:bg-softWhite"></span>
-        </button>
       </div>
+      <button id="menu-btn" className="z-30 mt-2 block md:hidden focus:outline-none hamburger" onClick={onClickMobileMenu}>
+        <span className="hamburger-top bg-grayishBlack dark:bg-softWhite"></span>
+        <span className="hamburger-middle bg-grayishBlack dark:bg-softWhite"></span>
+        <span className="hamburger-bottom bg-grayishBlack dark:bg-softWhite"></span>
+      </button>
 
     </div>
   );
